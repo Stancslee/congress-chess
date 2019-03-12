79c80ec813af0564b0d408d9069e235df148b5bf
Stanley Lee <stancslee@gmail.com>
2019-03-11 21:15:08 -0700

    Functional make_move and parse_move w/o error checking.

--- classes.py
+++ classes.py
@@ -8,9 +8,9 @@ class Board:
     def __init__(self):
         self.rows = 6
         self.cols = 8
-        self.board = self.generateBoard()
+        self.board = self.generate_board()
 
-    def generateBoard(self):
+    def generate_board(self):
         board = []
         # Initialize empty game board
         board = [ (['-'] * self.cols) for row in range(self.rows) ]
@@ -36,29 +36,27 @@ class Board:
             board[4][x+1] = 'p'
         return board
 
-    def printBoard(self):
+    def print_board(self):
         for x in range(self.rows):
             print( ('%s ' % str(self.rows - x)) + ' '.join(self.board[x]) )
         print('  ---------------')
         print('  A B C D E F G H')
 
-    def generateMoves(self):
-        legalMoves = []
+    def generate_moves(self):
+        legal_moves = []
+        return legal_moves
 
-    def getBoard(self):
+    def get_board(self):
         return self.board
 
-    """
-    def make_move(self, cur_move, board):
+    def make_move(self, cur_move):
         move = self.parse_move(cur_move)
-        print(move[1][0])
-        print(move[1][1])
         # Save src & dst values
-        print( "src = " + board[ move[0][0] ][ move[0][1] ] )
-        print( "dst = " + board[ move[1][0] ][ move[1][1] ] )
+        src = self.get_board()[ move[0][0] ][ move[0][1] ]
+        dst = self.get_board()[ move[1][0] ][ move[1][1] ]
         # Make move
-        board[ move[1][0] ][ move[1][1] ] = src
-        board[ move[0][0] ][ move[0][1] ] = '-'
+        self.get_board()[ move[1][0] ][ move[1][1] ] = src
+        self.get_board()[ move[0][0] ][ move[0][1] ] = '-'
 
     def parse_move(self, move):
         col_map = {
@@ -76,13 +74,17 @@ class Board:
             char.append(c)
         src = (self.rows - int(char[1]), col_map.get(char[0]))
         dst = (self.rows - int(char[3]), col_map.get(char[2]))
-        print(src, dst)
         return [src, dst]
-    """
 
     def king_gen_moves(pos):
         return
 
+    def pawn_gen_moves():
+        return
 
+    def horse_gen_moves():
+        return
 
+    def bishop_gen_moves():
+        return
 
--- congress-chess.py
+++ congress-chess.py
@@ -10,78 +10,45 @@ from classes import (
 
 def main():
     board = Board()
-    board.printBoard()
-    playerTurn = False
+    player_turn = False
     choice = True
-    turnChoice = ''
+    turn_choice = ''
     move = ''
-    gameOver = False
+    game_over = False
 
-    while(not gameOver):
+    while(not game_over):
         while(choice):
-            turnChoice = input("Please select your turn (1 or 2): ")
-            if(turnChoice == '1'):
-                playerTurn = True
+            turn_choice = input("Please select your turn (1 or 2): ")
+            if(turn_choice == '1'):
+                player_turn = True
                 choice = False
-            elif(turnChoice == '2'):
-                playerTurn = False
+            elif(turn_choice == '2'):
+                player_turn = False
                 choice = False
             else:
                 print('Your input is not an integer 1 or 2. Please try again.\n')
 
         # Call move Generator
-        print(board.getBoard()[0][0])
+        board.print_board()
         # Players take turns
-        if(playerTurn):
+        if(player_turn):
             #player makes move
             move = input('Enter your move: ')
             print('Your move: %s\n' % move)
-            make_move(move, board)
-            playerTurn = False
+            board.make_move(move)
+            player_turn = False
         else:
             # NPC Move Generator (returns list of legal moves)
             # legalMoves = moveGenerator()
 
             # NPC makes move
             print('Computer move: rand\n')
-            playerTurn = True
+            player_turn = True
 
         # Check legality of move
             # Compare list of legal moves to user input
         if(move == 'stop'):
-            gameOver = True
-
-def make_move(cur_move, board):
-    move = parse_move(cur_move, board)
-    print(move[1][0])
-    print(move[1][1])
-    # Save src & dst values
-    print( "src = " + board[ move[0][0] ][ move[0][1] ] )
-    print( "dst = " + board[ move[1][0] ][ move[1][1] ] )
-    # Make move
-    board[ move[1][0] ][ move[1][1] ] = src
-    board[ move[0][0] ][ move[0][1] ] = '-'
-
-def parse_move(move, board):
-    col_map = {
-        'A': 0,
-        'B': 1,
-        'C': 2,
-        'D': 3,
-        'E': 4,
-        'F': 5,
-        'G': 6,
-        'H': 7
-    }
-    char = []
-    for c in move:
-        char.append(c)
-        print(c)
-    src = (board.rows - int(char[1]), col_map.get(char[0]))
-    dst = (board.rows - int(char[3]), col_map.get(char[2]))
-    print(src, dst)
-    return [src, dst]
-
+            game_over = True
 
 if __name__ == "__main__":
     main()
